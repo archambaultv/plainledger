3a89c80d9e71b53fe7f6767cfc94602d7481abbc
Vincent Archambault-B <vincentarchambault@icloud.com>
2020-12-10 01:03:21 -0500

    Allow multiple source position for the same error.

--- src/Plainledger/Error.hs
+++ src/Plainledger/Error.hs
@@ -11,8 +11,10 @@
 module Plainledger.Error
 (
   Error(..),
+  ErrorType(..),
   mkErrorPos,
   mkError,
+  addErrorPos,
   printError,
   printErrorType,
 ) where
@@ -20,33 +22,47 @@ module Plainledger.Error
 import Plainledger.SourcePos
 
 -- | Errors, with or without a source file
-data Error = Error (Maybe SourcePos) ErrorType
+data Error = Error [SourcePos] ErrorType
 
 -- | List all possibles error in plainledger
 data ErrorType
-  = UnknownError String
+  = CustomErr String
+  | DuplicateAccountIdErr
+  | ParseDateErr String
 
 mkErrorPos :: SourcePos -> ErrorType -> Error
-mkErrorPos pos e -> Error (Just pos) e
+mkErrorPos pos e -> Error [pos] e
 
 mkError :: ErrorType -> Error
 mkError e = Error Nothing e
 
--- | Pretty print the error message by adding the source file information
-printError :: Error -> String
-printError (Just pos) e | spRow pos <= 0 
-  = spFile pos 
-  ++ "error:\n" 
-  ++ printErrorType e
-printError (Just pos) e
-  = spFile pos
-  ++ ":"
-  ++ (show $ spRow pos)
-  ++ ":"
-  ++ (show $ spColumn pos)
-  ++ "error:\n"
-  ++ printErrorType e
-printError Nothing e = "error:\n" ++ printErrorType e
+mkCustomErr :: String -> Error
+mkCustomErr s =  Error [] (CustomErr s)
 
+addErrorPos :: SourcePos -> Error -> Error
+addErrorPos s (Error p e) = Error (s:p) e
+
+-- | Pretty print the error message and add the source file information
+printError :: Error -> String
+printError (Error [] e) = "error:\n" ++ printErrorType e
+printError (Error [pos] e) = showSourcePos pos ++ " error:\n" ++ printErrorType e
+printError (Error (p:ps) e) 
+  = printError (Error [p] e)
+  ++ "error also located at:\n  "
+  ++ intercalate "\n  " (map showSourcePos ps)
+
+-- | Pretty print the error message
 printErrorType :: Error -> String
-printErrorType (UnknownError s) = s
+printErrorType (CustomErr s) = s
+printErrorType (ParseDateErr s) 
+  = "Unable to parse date \"" ++ s ++ "\".\nDate must be in the YYYY-MM-DD format"
+printErrorType _ = "Unknown error"
+
+showSourcePos :: SourcePos -> String
+showSourcePos (SourcePos f r _) | r <= 0 = f
+showSourcePos (SourcePos f r c)
+  = f
+  ++ ":"
+  ++ show r
+  ++ ":"
+  ++ show c
