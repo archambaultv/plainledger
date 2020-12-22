e1eb0ff94b71b0692a2706d9db632142fc6efa12
Vincent Archambault-B <vincentarchambault@icloud.com>
2020-12-22 13:28:14 -0500

    More unittest for JournalFile

--- test/Journal/Journal.hs
+++ test/Journal/Journal.hs
@@ -16,22 +16,28 @@ journalTestTree = testGroup "Journal tests"
 journalFileTestTree :: TestTree
 journalFileTestTree =
   testGroup "JournalFile"
-    [ okConfig "Journal-01.csv" -- With BOM
+    [ -- With BOM, comma for csv, period for decimal
+      okConfig "Journal-01.csv" 
         $ JournalFile "Solde d'ouverture" "Bénéfice" "My Company" '.' ',' 7 "comptes.csv" 
           ["transactions.csv"] ["vérification de soldes.csv"],
-      okConfig "Journal-02.csv" -- With BOM
+      -- With BOM, semicolon for csv, comma for decimal
+      okConfig "Journal-02.csv" 
         $ JournalFile "Solde d'ouverture" "Bénéfice" "My Company" ',' ';' 1 "comptes.csv" 
           ["transactions.csv"] ["vérification de soldes.csv"],
-      okConfig "Journal-03.csv" -- Without BOM
+      -- Without BOM, comma for csv, period for decimal
+      okConfig "Journal-03.csv" 
         $ JournalFile "Solde d'ouverture" "Bénéfice" "My Company" '.' ',' 7 "comptes.csv" 
           ["transactions.csv"] ["vérification de soldes.csv"],
-      okConfig "Journal-04.csv" -- Without BOM
+      -- Without BOM, comma for csv, period for decimal, multiple files
+      okConfig "Journal-04.csv" 
         $ JournalFile "Solde d'ouverture" "Bénéfice" "My Company, Inc." '.' ',' 1 "comptes.csv" 
           ["transactions 1.csv", "transactions 2.csv", "transactions 3.csv"] 
           ["soldes 1.csv", "soldes 2.csv"],
-      okConfig "Journal-05.csv" -- With BOM
+      -- With BOM, tab for csv, period for decimal
+      okConfig "Journal-05.csv"
         $ JournalFile "Solde d'ouverture" "Bénéfice" "My Company" '.' '\t' 7 "comptes.csv" 
           ["transactions.csv"] ["vérification de soldes.csv"],
+
       koConfig "Journal-06.csv"
         $ mkError (SourcePos "test/Journal/JournalFile/Journal-06.csv" 1 0 ) InvalidHeaderJournalFile,
       koConfig "Journal-07.csv"
@@ -41,7 +47,16 @@ journalFileTestTree =
         (EmptyFieldInJournalFile "Compte pour les soldes d'ouverture"),
       koConfig "Journal-09.csv"
         $ mkError (SourcePos "test/Journal/JournalFile/Journal-09.csv" 0 0 ) 
-        (MissingFieldinJournalFile "Compte pour les soldes d'ouverture")
+        (MissingFieldinJournalFile "Compte pour les soldes d'ouverture"),
+      koConfig "Journal-10.csv"
+        $ mkError (SourcePos "test/Journal/JournalFile/Journal-10.csv" 0 0 ) 
+        (MissingFieldinJournalFile "Compte pour les bénéfices"),
+      koConfig "Journal-11.csv"
+        $ mkError (SourcePos "test/Journal/JournalFile/Journal-11.csv" 0 0 ) 
+        (MissingFieldinJournalFile "Nom"),
+      koConfig "Journal-12.csv"
+        $ mkError (SourcePos "test/Journal/JournalFile/Journal-12.csv" 0 0 ) 
+        (MissingFieldinJournalFile "Fichier des comptes")
     ]
 
 okConfig :: String -> JournalFile -> TestTree
