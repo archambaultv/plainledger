8c3bd0dd82853cb4f1d76443ed1e3c66ede0269a
Vincent Archambault-B <vincentarchambault@icloud.com>
2020-12-25 02:13:41 -0500

    Fix exponent sign parsing

--- src/Plainledger/Journal/Amount.hs
+++ src/Plainledger/Journal/Amount.hs
@@ -39,6 +39,7 @@ type Quantity = Decimal
 -- 12e12
 -- 12.10E12
 -- 16e-24
+-- 16e+24
 -- 45.24e48
 -- .246E7
 -- -2e5
@@ -47,18 +48,18 @@ type Quantity = Decimal
 parseAmount :: forall m . (MonadError Errors m) => Char -> T.Text -> m Quantity
 parseAmount sep x = do
   let err = (throwError $ mkErrorNoPos $ ParseAmountErr $ T.unpack x)
-  (isPositive, x1) <- parseSign x
+  (coeffSign, x1) <- parseSign x
   (coeff, x2) <- parseCoeff x1
   (frac, x3) <- parseFractional x2
-  (e2, x4) <- parseExponent x3
+  (e2, expSign, x4) <- parseExponent x3
+
   when (not $ T.null x4) err
   let nText = T.append coeff frac
   when (T.null nText) err
-  let n = if isPositive
-          then strToInt nText
-          else negate $ strToInt nText
+  let n = applySign coeffSign $ strToInt nText
   -- e means the number of decimal digits
-  let e = T.length frac - strToInt e2
+  -- negative value means we need to add some zeros to n
+  let e = T.length frac - (applySign expSign $ strToInt e2)
   if e < 0
      then return $ Decimal 0 $ n * 10 ^ negate e
      else if e < 256
@@ -67,20 +68,17 @@ parseAmount sep x = do
 
 
   where parseSign :: T.Text -> m (Bool, T.Text)
-        parseSign "" = throwError $ mkErrorNoPos $ ParseAmountErr $ T.unpack x
         parseSign y | T.head y == '-' = return (False, T.drop 1 y)
         parseSign y | T.head y == '+' = return (True, T.drop 1 y)
         parseSign y = return (True, y)
 
         parseCoeff :: T.Text -> m (T.Text, T.Text)
-        parseCoeff "" = throwError $ mkErrorNoPos $ ParseAmountErr $ T.unpack x
         parseCoeff y = 
           let n = T.takeWhile isDigit y
               r = T.drop (T.length n) y
           in return (n, r)
 
         parseFractional :: T.Text -> m (T.Text, T.Text)
-        parseFractional "" = return ("", "")
         parseFractional y | T.head y == sep =
           let n = T.takeWhile isDigit (T.drop 1 y)
               r = T.drop (T.length n + 1) y
@@ -89,16 +87,21 @@ parseAmount sep x = do
              else return (n, r)
         parseFractional y = return ("", y)
 
-        parseExponent :: T.Text -> m (T.Text, T.Text)
-        parseExponent "" = return ("","")
-        parseExponent y | T.head y == 'e' || T.head y == 'E' =
-           let n = T.takeWhile isDigit (T.drop 1 y)
-               r = T.drop (T.length n) y
-           in return (n, r)
-        parseExponent _ = throwError $ mkErrorNoPos $ ParseAmountErr $ T.unpack x
+        parseExponent :: T.Text -> m (T.Text, Bool, T.Text)
+        parseExponent y | T.head y == 'e' || T.head y == 'E' = do
+          (isPos, x1) <- parseSign (T.drop 1 y)
+          (n, x2) <- parseCoeff x1
+          if T.length n == 0
+             then throwError $ mkErrorNoPos $ ParseAmountErr $ T.unpack x
+             else return (n, isPos, x2)
+        parseExponent y = return ("", True, y)
 
         strToInt :: (Integral n) => T.Text -> n
         strToInt y = T.foldl' 
                      (\t v -> 10 * t + (fromIntegral $ ord v - ord '0')) 
                      0
                      y
+
+        applySign :: (Integral n) => Bool -> n -> n
+        applySign True n = n
+        applySign False n = negate n
